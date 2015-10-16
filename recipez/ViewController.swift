eba2a71c1f7e341305839647fde5c0663c4e1cba
rmarcha06 <rmarcha06@gmail.com>
2015-10-16 15:09:14 -0500

    App finished

--- recipez/ViewController.swift
+++ recipez/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -14,11 +15,34 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var recipes = [Recipe]()
 
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        fetchAndSetResults()
+        tableView.reloadData()
+    }
+    
+    func fetchAndSetResults() {
+        let app = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context = app.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Recipe")
+        
+        do {
+            let results = try context.executeFetchRequest(fetchRequest)
+            self.recipes = results as! [Recipe]
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
         
     }
 
